b0baf50377bd7ed48b21d5b112240eb725dc3828
chhan <chhan@ebrainsoft.com>
2022-09-01 17:54:10 +0900

    [feature] common components

--- front-admin/src/App.vue
+++ front-admin/src/App.vue
@@ -9,6 +9,15 @@
       </component>
 
       <!-- global ui -->
+      <!-- msg -->
+      <WDMessageBox ref="confirmModal" />
+
+      <!-- loading -->
+      <b-overlay :show="showLoading"
+                 spinner-variant="primary"
+                 spinner-type="grow"
+                 no-fade
+                 no-wrap/>
 
   </div>
 </template>
@@ -25,11 +34,51 @@
 <script>
 
 import EnumLayout from "@/plugins/layout/enum-layout";
+import WDMessageBox from "@/components/WDMessageBox";
+import {EventBus} from "@/plugins/EventBus";  // TODO/remove event bus
+
 export default {
+    components: { WDMessageBox },
+    created() {
+        this.initMessageEvents();
+        this.initLoadingEvents();
+    },
+    mounted() {
+    },
+    data(){
+        return {
+            showLoading: false,
+        }
+    },
     computed: {
         layout() {
             return (this.$route.meta.layout || EnumLayout.DefaultLayout);
         }
+    },
+    methods: {
+        initLoadingEvents() {
+            EventBus.$on("@showLoading", () => {
+                this.showLoading = true;
+            });
+            EventBus.$on("@hideLoading", () => {
+                this.showLoading = false;
+            });
+        },
+
+        initMessageEvents() {
+            EventBus.$on("@showAlert", ({title, content, callback, okBtnName}) => {
+                this.$refs.confirmModal.show({title, content, callback, okBtnName, showCancel:false, isBtnX:false});
+            });
+            EventBus.$on("@showConfirm", ({title, content, callback, okBtnName, cancelBtnName}) => {
+                this.$refs.confirmModal.show({title, content, callback, okBtnName, cancelBtnName, isBtnX:false});
+            });
+            EventBus.$on("@showContent", ({title, component}) => {
+                this.$refs.confirmModal.show({title, component, showOk:false, showCancel:false, isBtnX:false});
+            });
+            EventBus.$on("@hideContent", () => {
+                this.$refs.confirmModal.close();
+            });
+        },
     },
 }
 </script>
--- front-admin/src/views/Home.vue
+++ front-admin/src/views/Home.vue
@@ -3,16 +3,29 @@
         <img alt="Vue logo" src="../assets/logo.png">
         <p>Welcome to Your Vue.js App</p>
         <p>
-            <WDButton >hello</WDButton>
+            <WDButton @click=" onClick " >hello</WDButton>
         </p>
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
+
+import {EventBus} from "@/plugins/EventBus";
 
 export default {
     name: 'Home',
-    components: {}
+    components: {},
+    methods: {
+        onClick() {
+            this.$showLoading();
+            this.$showAlert(
+                {
+                    title: '알림',
+                    content: `test 을(를) 입력해 주세요.`,
+                    callback: () => { this.$hideLoading(); },
+                    okBtnName: '확인'
+                });
+        },
+    }
 }
 </script>
